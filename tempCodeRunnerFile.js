ced444224897145e2baf1897539e32ab8699ee23
yomidep <noabiliaminfirst@gmail.com>
2024-07-25 17:06:17 +0100

    Excercis done

--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-transaction.add(
-        //     SystemProgram.transfer({
-        //         fromPubkey: from.publicKey,
-        //         toPubkey: to,
-        //         lamports: balance * 0.1 - fee,
-        //     })
-        // )
